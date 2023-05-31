8cb85270bb5792e6579065396d91d7c59a745a55
Maxim-10 <133491976+Maxim-10@users.noreply.github.com>
2023-06-01 02:48:12 +0300

    Update SquareAnimator.java

--- src/org/dstu/shapes/animation/SquareAnimator.java
+++ src/org/dstu/shapes/animation/SquareAnimator.java
@@ -4,6 +4,8 @@ import org.dstu.shapes.Shape;
 import org.dstu.shapes.Square;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
 
 public class SquareAnimator extends Animator {
 
@@ -14,10 +16,12 @@ public class SquareAnimator extends Animator {
     @Override
     public void run() {
         while (true) {
-            if (shape.getX() < (800 - shape.getSide()) && shape.getY() == 0) shape.moveTo(shape.getX() + 10, shape.getY());
-            else if (shape.getX() == (800 - shape.getSide()) && shape.getY() < (600 - shape.getSide())) shape.moveTo(shape.getX(), shape.getY() + 10);
-            else if ((shape.getX() > 0) && shape.getY() == (600 - shape.getSide())) shape.moveTo(shape.getX() - 10, shape.getY());
-            else if (shape.getX() == 0 && shape.getY() > 0) shape.moveTo(shape.getX(), shape.getY() - 10);
+            System.out.println(shape.getX() + "   " + shape.getY());
+            if (shape.getX() < (component.getWidth() - shape.getSide()) && shape.getY() == 0) shape.moveTo(shape.getX() + 10, shape.getY());
+            else if (shape.getX() >= (component.getWidth() - shape.getSide()) && shape.getY() < (component.getHeight() - shape.getSide())) shape.moveTo(shape.getX(), shape.getY() + 10);
+            else if ((shape.getX() > 0) && shape.getY() >= (component.getHeight() - shape.getSide())) shape.moveTo(shape.getX() - 10, shape.getY());
+            else if (shape.getX() <= 0 && shape.getY() > 0) shape.moveTo(shape.getX(), shape.getY() - 10);
+            changeColor();
 
             if (component != null) {
                 component.repaint();
@@ -27,6 +31,16 @@ public class SquareAnimator extends Animator {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    public void changeColor() {
+        if ((shape.getX() >= (component.getSize().width - shape.getSide()) && shape.getY() == 0) ||
+                shape.getX() >= (component.getSize().width - shape.getSide()) && shape.getY() >= (component.getSize().height - shape.getSide()) ||
+                ((shape.getX() <= 0) && shape.getY() >= (component.getSize().height - shape.getSide())) ||
+                shape.getX() <= 0 && shape.getY() <= 0) {
+            Random rand = new Random();
+            shape.setColor(new Color(rand.nextInt(256), rand.nextInt(256), rand.nextInt(256)));
         }
     }
 }
